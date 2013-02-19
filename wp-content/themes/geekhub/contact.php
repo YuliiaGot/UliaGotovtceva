62208221065538d8cf0f5e6f7bb0655c1a7bb27d
Ulia Gotovtceva <barelyonmars@gmail.com>
2013-02-19 18:46:13 +0200

    new

--- wp-content/themes/geekhub/contact.php
+++ wp-content/themes/geekhub/contact.php
@@ -5,25 +5,9 @@ Template Name: Contact
 
 get_header(); ?>
 
-<div id="contacts" class="content">
-    <h2>Свяжитесь с нами</h2>
-    <div class="content-left">
-        <h3>Заходьте</h3>
-        <img src="wp-content/themes/geekhub/images/contacts/map.png" alt="map" />
-    </div>
-    <dl>
-        <dt>Звоніть</dt>
-        <dd>+38 097 977 01 87</dd>
-        <dt>Пишіть</dt>
-        <dd><a href="mailto:info@geekhub.com">info@geekhub.com</a></dd>
-        <dt>Слідкуйте</dt>
-        <dd><ul>
-            <li><a class="big-fb" href="#"></a></li>
-            <li><a class="big-vk" href="#"></a></li>
-            <li><a class="big-tw" href="#"></a></li>
-        </ul></dd>
-    </dl>
-</div>
-<!--content_end-->
+<?php
+$content_post = get_post('93');
+echo $content_post->post_content;
+?>
 
 <?php get_footer(); ?>
--- wp-content/themes/geekhub/team.php
+++ wp-content/themes/geekhub/team.php
@@ -5,43 +5,9 @@ Template Name: Team
 
 get_header(); ?>
 
-<div id="team" class="content">
-    <h2>Знай наших!</h2>
-    <ul>
-        <li>
-            <div class="developer">
-                <h3>Хытыемель<br />Цыэсесенко</h3>
-                <span>Всея кодудер</span>
-            </div>
-            <img src="wp-content/themes/geekhub/images/team/team_pic.png" alt="photo" />
-            <p>Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
-        </li>
-        <li class="right">
-            <div class="developer">
-                <h3>Хытыемель<br />Цыэсесенко</h3>
-                <span>Всея кодудер</span>
-            </div>
-            <img src="wp-content/themes/geekhub/images/team/team_pic.png" alt="photo" />
-            <p>Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
-        </li>
-        <li>
-            <div class="developer">
-                <h3>Хытыемель<br />Цыэсесенко</h3>
-                <span>Всея кодудер</span>
-            </div>
-            <img src="wp-content/themes/geekhub/images/team/team_pic.png" alt="photo" />
-            <p>Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
-        </li>
-        <li class="right">
-            <div class="developer">
-                <h3>Хытыемель<br />Цыэсесенко</h3>
-                <span>Всея кодудер</span>
-            </div>
-            <img src="wp-content/themes/geekhub/images/team/team_pic.png" alt="photo" />
-            <p>Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
-        </li>
-    </ul>
-</div>
-<!--content_end-->
+<?php
+$content_post = get_post('96');
+echo $content_post->post_content;
+?>
 
 <?php get_footer(); ?>
